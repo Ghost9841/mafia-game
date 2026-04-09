7e808efeae6c3bafa897169731ea54ed8a10c9a9
Ghost Aakash <88389544+Ghost9841@users.noreply.github.com>
2026-04-09 08:53:10 +0000

    Pushing Everything then

--- server/services/socketServerService.js
+++ server/services/socketServerService.js
@@ -4,13 +4,14 @@ import { Server } from 'socket.io';
 export const createSocketServer = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: ["http://localhost:5173","https://refactored-engine-67447vx7pq5cr567-5173.app.github.dev/"  ],
       methods: ["GET", "POST"]
     }
   });    
   
   io.on('connection', (socket) => {
         console.log('a user connected');
+      
     });
 
 server.listen(3000, () => {
