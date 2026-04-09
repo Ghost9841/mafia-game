20b18b20080ac308c21c2cb459b54c34a36122f7
Ghost Aakash <88389544+Ghost9841@users.noreply.github.com>
2026-04-09 09:15:27 +0000

    Fixed this Indexing Problem brev

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,40 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { socket } from './server';
-
-export default function App() {
-  const [isConnected, setIsConnected] = useState(socket.connected);
-  const [fooEvents, setFooEvents] = useState([]);
-
-  useEffect(() => {
-    function onConnect() {
-      setIsConnected(true);
-    }
-
-    function onDisconnect() {
-      setIsConnected(false);
-    }
-
-    function onFooEvent(value) {
-      setFooEvents(previous => [...previous, value]);
-    }
-
-    socket.on('connect', onConnect);
-    socket.on('disconnect', onDisconnect);
-    socket.on('foo', onFooEvent);
-
-    return () => {
-      socket.off('connect', onConnect);
-      socket.off('disconnect', onDisconnect);
-      socket.off('foo', onFooEvent);
-    };
-  }, []);
-
+export const App=()=>{
   return (
-    <div className="App">
-      <ConnectionState isConnected={ isConnected } />
-      <Events events={ fooEvents } />
-      <ConnectionManager />
-      <MyForm />
+    <div>
+      <h1 className="text-black">Welcome to Mafia Game!</h1>
     </div>
-  );
+  )
 }
+
+export default App;
