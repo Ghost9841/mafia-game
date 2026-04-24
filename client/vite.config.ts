33cc082196600803dc6b2080a7e603b60c2b4874
Ghost Aakash <88389544+Ghost9841@users.noreply.github.com>
2026-04-24 07:39:35 +0000

    it better work this time

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,17 +2,10 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        navigateFallback: '/offline.html'
-      }
-    })
   ],
   resolve: {
     alias: {
